1b8594229eb685300fab1a17202f83ed40eb3331
atcook65 <71952710+atcook65@users.noreply.github.com>
2023-09-14 23:01:23 -0500

    Update script.js
    update

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
-i18next.init({
-  lng: 'en', // Set the default language
-  resources: {
-    en: {
-      translation: {
-        // Load English translations from en.json
-      }
-    },
-    es: {
-      translation: {
-        // Load Spanish translations from es.json
-      }
-    }
-  }
-});
-// Change the language when a user selects a different option
-document.getElementById('language-selector').addEventListener('change', function () {
-  const selectedLanguage = this.value;
-  i18next.changeLanguage(selectedLanguage);
-});
+// Define a variable to store the selected language (default to English)
+let currentLanguage = 'en';
 
-i18next.on('languageChanged', (lng) => {
-  document.getElementById('welcome_message').textContent = i18next.t('welcome_message');
-  document.getElementById('about_us').textContent = i18next.t('about_us');
-  document.getElementById('contact_us').textContent = i18next.t('contact_us');
-  // Update other elements as needed
-});
+// Function to change the language
+function changeLanguage(language) {
+    currentLanguage = language;
+    translatePage();
+}
+
+// Function to translate the page content
+function translatePage() {
+    fetch(`${currentLanguage}.json`)
+        .then(response => response.json())
+        .then(data => {
+            // Replace content with translated text
+            document.getElementById('welcome_message').textContent = data.welcome_message;
+            document.getElementById('about_us').textContent = data.about_us;
+            document.getElementById('contact_us').textContent = data.contact_us;
+            // Add more elements as needed
+        });
+}
+
+// Initialize translation
+translatePage();
