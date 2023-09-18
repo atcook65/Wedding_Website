891e3fd82e22892941e458c6725fc940906fc4b7
atcook65 <71952710+atcook65@users.noreply.github.com>
2023-09-17 23:09:40 -0500

    Update script2.js
    fix

--- script2.js
+++ script2.js
@@ -1,8 +1,15 @@
 // JavaScript code for image enlargement
 const imagesToEnlarge = document.querySelectorAll('.enlarge');
+const enlargedImageContainer = document.querySelector('.enlarged-image-container');
+const enlargedImage = document.querySelector('.enlarged-image');
 
 imagesToEnlarge.forEach(image => {
     image.addEventListener('click', () => {
-        image.classList.toggle('enlarged');
+        enlargedImage.src = image.src;
+        enlargedImageContainer.style.display = 'flex';
     });
 });
+
+function closeEnlargedImage() {
+    enlargedImageContainer.style.display = 'none';
+}
