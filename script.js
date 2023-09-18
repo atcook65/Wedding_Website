df34739ceba8c87adbad35745a7384739cc8d919
atcook65 <71952710+atcook65@users.noreply.github.com>
2023-09-18 09:56:31 -0500

    Update script.js
    nexy

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function translatePage() {
         .then(data => {
             // Replace content with translated text
             document.getElementById('HOTEL').textContent = data.HOTEL;
+            document.getElementById('WEDDING_DAY').textContent = data.WEDDING_DAY;
             document.getElementById('PERU').textContent = data.PERU;
             document.getElementById('CUSCO').textContent = data.CUSCO;
             document.getElementById('ABOUT_US').textContent = data.ABOUT_US;
