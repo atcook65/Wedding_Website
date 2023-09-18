ac2bc21e2f39c7179994d6139a85b2072535a849
atcook65 <71952710+atcook65@users.noreply.github.com>
2023-09-17 22:47:28 -0500

    Update script2.js
    fix

--- script2.js
+++ script2.js
@@ -1,35 +1,8 @@
-function changeLanguage(language) {
-    i18next.changeLanguage(language);
-}
+// JavaScript code for image enlargement
+const imagesToEnlarge = document.querySelectorAll('.enlarge');
 
-i18next.init({
-    lng: 'en', // Set the default language
-    resources: {
-        en: {
-            translation: {
-                welcome_message: 'Welcome to our website!',
-                about_us: 'About Us',
-                contact_us: 'Contact Us',
-                // Add more translations for English
-            }
-        },
-        es: {
-            translation: {
-                welcome_message: 'Bienvenidos a nuestro sitio web!',
-                about_us: 'Sobre nosotros',
-                contact_us: 'Contáctenos',
-                // Add more translations for Spanish
-            }
-        }
-    }
-}, function(err, t) {
-    // Initialize translations
-    updateContent();
+imagesToEnlarge.forEach(image => {
+    image.addEventListener('click', () => {
+        image.classList.toggle('enlarged');
+    });
 });
-
-function updateContent() {
-    document.getElementById('welcome_message').textContent = i18next.t('welcome_message');
-    document.getElementById('about_us').textContent = i18next.t('about_us');
-    document.getElementById('contact_us').textContent = i18next.t('contact_us');
-    // Update more content as needed
-}
