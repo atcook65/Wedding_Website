b1c6d95e88010c76fdf296f697c44506bf2478cc
atcook65 <71952710+atcook65@users.noreply.github.com>
2023-10-08 22:19:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,11 @@ function translatePage() {
         .then(data => {
             // Replace content with translated text
             document.getElementById('DATE').textContent = data.DATE;
+            document.getElementById('WELCOME').textContent = data.WELCOME;
             document.getElementById('DATE2').textContent = data.DATE2;
+            document.getElementById('WEDDING').textContent = data.WEDDING;
+            document.getElementById('DATE3').textContent = data.DATE3;
+            document.getElementById('RECEPTION').textContent = data.RECEPTION;
             document.getElementById('HOTEL').textContent = data.HOTEL;
             document.getElementById('WEDDING_DAY').textContent = data.WEDDING_DAY;
             document.getElementById('PERU').textContent = data.PERU;
