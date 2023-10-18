9fa6a0c611a6421e55e3ea0b1d1e602e676df9d2
atcook65 <71952710+atcook65@users.noreply.github.com>
2023-10-18 12:00:39 -0400

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,15 +1,2 @@
-// JavaScript code for image enlargement
-const imagesToEnlarge = document.querySelectorAll('.enlarge');
-const enlargedImageContainer = document.querySelector('.enlarged-image-container');
-const enlargedImage = document.querySelector('.enlarged-image');
-
-imagesToEnlarge.forEach(image => {
-    image.addEventListener('click', () => {
-        enlargedImage.src = image.src;
-        enlargedImageContainer.style.display = 'flex';
-    });
-});
-
-function closeEnlargedImage() {
-    enlargedImageContainer.style.display = 'none';
-}
+// This is where you could add specific interactive or dynamic behaviors.
+// The Lightbox library will handle most of the image modal work, though.
