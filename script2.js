f9a199412dee489933570d4de56f3b2841afbabc
atcook65 <71952710+atcook65@users.noreply.github.com>
2023-10-22 17:30:31 -0400

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,2 +1,22 @@
 // This is where you could add specific interactive or dynamic behaviors.
 // The Lightbox library will handle most of the image modal work, though.
+// Existing functions remain unchanged
+function openModal(imageSrc) {
+    var modal = document.getElementById("myModal");
+    var modalImg = document.getElementById("modalImage");
+    modal.style.display = "block";
+    modalImg.src = imageSrc;
+}
+
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
+
+// New: Close the modal if someone clicks outside the modal content
+window.onclick = function(event) {
+    var modal = document.getElementById("myModal");
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
