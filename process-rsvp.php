113771fda726f86e3f46d5900a234ca72467ada3
atcook65 <71952710+atcook65@users.noreply.github.com>
2023-11-05 16:36:31 -0500

    Update process-rsvp.php

--- process-rsvp.php
+++ process-rsvp.php
@@ -1,38 +1,63 @@
 <?php
+
+// Only process POST reqeusts.
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $attendance = $_POST['attendance'];
-    $guests = $_POST['guests'];
-    $message = $_POST['message'];
-    
-    $to = 'atcook65@gmail.com, ricardoachata@gmail.com'; // your email
-    $subject = 'Wedding RSVP';
-    $content = "Name: $name\n";
-    $content .= "Email: $email\n";
-    $content .= "Attendance: $attendance\n";
-    $content .= "Number of Guests: $guests\n";
-    $content .= "Message: $message";
-    $headers = 'From: webmaster@example.com' . "\r\n" .
-               'Reply-To: ' . $email . "\r\n" .
-               'X-Mailer: PHP/' . phpversion();
+    // Get the form fields and remove whitespace.
+    $name = strip_tags(trim($_POST["name"]));
+    $name = str_replace(array("\r","\n"),array(" "," "),$name);
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $attendance = trim($_POST["attendance"]);
+    $guests = trim($_POST["guests"]);
+    $message = trim($_POST["message"]);
 
-    mail($to, $subject, $content, $headers);
+    // Check that data was sent to the mailer.
+    if ( empty($name) OR empty($attendance) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        // Set a 400 (bad request) response code and exit.
+        http_response_code(400);
+        echo "Oops! There was a problem with your submission. Please complete the form and try again.";
+        exit;
+    }
 
-// Simple validation example
-if (empty($name) || empty($email) || empty($attendance) || empty($guests)) {
-    // Handle the error - you should redirect or inform the user
-    die('Please fill in all required fields.');
+    // Set the recipient email addresses
+    $recipient = "atcook65@gmail.com"; // TODO: Update this to your desired email address.
+    $recipient2 = "ricardoachata@gmail.com"; // TODO: Update this to your partner's email address.
+
+    // Set the email subject.
+    $subject = "New RSVP from $name";
+
+    // Build the email content.
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n";
+    $email_content .= "Attendance: $attendance\n";
+    $email_content .= "Number of Guests: $guests\n\n";
+    $email_content .= "Message:\n$message\n";
+
+    // Build the email headers.
+    $email_headers = "From: $name <$email>";
+
+    // Send the email to the first recipient.
+    $email_sent = mail($recipient, $subject, $email_content, $email_headers);
+
+    // Send the email to the second recipient if the first was successful.
+    if($email_sent) {
+        mail($recipient2, $subject, $email_content, $email_headers);
+    }
+
+    // Check if the mail was sent and respond accordingly
+    if ($email_sent) {
+        // Set a 200 (okay) response code.
+        http_response_code(200);
+        echo "Thank You! Your RSVP has been sent.";
+    } else {
+        // If mail was not sent, there might be a server issue.
+        http_response_code(500);
+        echo "Oops! Something went wrong and we couldn't send your message.";
+    }
+
+} else {
+    // Not a POST request, set a 403 (forbidden) response code.
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
 }
 
-// Send the email and check if it was successful
-if (!mail($to, $subject, $content, $headers)) {
-    // Handle the error - mail function failed to send the email
-    die('Unable to send RSVP. Please try again later.');
-}
-
-// If mail was successful, redirect or inform the user
-echo 'Thank you for your RSVP!';
-
-}
 ?>
