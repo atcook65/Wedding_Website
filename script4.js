17d38ff1e8135c5f4e8701224503bc3d51b7a4c4
atcook65 <71952710+atcook65@users.noreply.github.com>
2024-05-19 18:10:05 -0400

    Update script4.js

--- script4.js
+++ script4.js
@@ -3,10 +3,12 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const showEvents = () => {
         let scrollY = window.scrollY;
-        events.forEach(event => {
+        events.forEach((event, index) => {
             let eventOffsetTop = event.offsetTop;
             if (scrollY + window.innerHeight > eventOffsetTop + event.offsetHeight / 2) {
-                event.classList.add("visible");
+                setTimeout(() => {
+                    event.classList.add("visible");
+                }, index * 200); // Delay each event's appearance
             }
         });
     };
